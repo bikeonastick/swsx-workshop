83d153ff0c6052b7abedd6b9816869259c8b932d
Brad Marsh <brad.marsh@reachlocal.com>
2014-02-26 15:52:51 -0600

    added show page

---
+++ app/assets/js/pages/show.js
@@ -0,0 +1,40 @@
+//  This will be called by the included PAGE.js as part of the liger initialization.
+PAGE.show = function(){
+	SHOW.initialize();
+}
+
+//  All of the code unique to the page's functionality
+var SHOW = {
+
+	show: null,
+
+	initialize: function(){
+		var me = this;
+
+		if(PAGE.args){
+			if("show" in PAGE.args){
+				me.show = PAGE.args.show;
+				me.displayDetails();
+				me.addBindings();
+			}
+		}
+
+		console.log("SHOW.initailize succesful");
+	},
+
+	displayDetails: function(){
+		var me = this;
+
+		var template = $("#showDetailsTemplate").html();
+		$("#page-content").append(_.template(template,{show:me.show}));
+	},
+
+	addBindings: function(){
+		var me = this;
+
+		$(".series-info").click(function(){
+			PAGE.openPage('Series','series',{'series_id': me.show.program.programme.programme.pid });
+		});
+	}
+
+}
