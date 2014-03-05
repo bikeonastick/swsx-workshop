a0fd7e9b8fa244d947281c9cf9a33d5eff6ca3d3
Brad Marsh <brad.marsh@reachlocal.com>
2014-03-05 15:21:03 -0600

    added about page

---
+++ app/assets/js/pages/about.js
@@ -0,0 +1,26 @@
+//  This will be called by the included PAGE.js as part of the liger initialization.
+PAGE.about = function(){
+    ABOUT.initialize();
+}
+
+//  All of the code unique to the page's functionality
+var ABOUT = {
+    initialize: function(){
+        var me = this;
+
+        /* 
+        *
+        *    All arguments that have been passed to this page are now ready to be accessed.
+        *    They can be found in the PAGE.args object.  
+        *
+        *
+        */
+
+        var template = $("#aboutTemplate").html();
+        $("#page-content").append(_.template(template,{}));
+
+        console.log("ABOUT.initailize succesful");
+    }
+
+    // lots of other cool stuff
+}
